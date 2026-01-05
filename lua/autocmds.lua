9b9e2e111417c38bbc1af79a311d499e02354247
Younes <younes.elhjouji@gmail.com>
2026-01-05 09:37:20 -0800

    close quickfix menu on exit focuss

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,11 +1,16 @@
 require "nvchad.autocmds"
 local autocmd = vim.api.nvim_create_autocmd
 
--- Auto-close nvim-tree when it loses focus
 autocmd("BufLeave", {
   callback = function()
+    -- Auto-close nvim-tree when it loses focus
     if vim.bo.filetype == "NvimTree" then
       require("nvim-tree.api").tree.close()
+    end
+
+    -- Auto-close quickfix when it loses focus
+    if vim.bo.filetype == "qf" then
+      vim.cmd "cclose"
     end
   end,
 })
